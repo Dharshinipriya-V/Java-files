2ee4a5c95c213f7292c93428537cd74510a65de3
Dharshinipriya-V <131668975+Dharshinipriya-V@users.noreply.github.com>
2024-08-30 09:27:52 +0530

    Delete csv directory

--- csv/src/main/java/com/kgisl/csv/StudentDao.java
+++
@@ -1,9 +0,0 @@
-package com.kgisl.csv;
-import java.util.List;
-
-public interface StudentDao {
-   public List<Student> getAllStudents();
-   public Student getStudent(int rollNo);
-   public void updateStudent(Student student);
-   public void deleteStudent(Student student);
-}
